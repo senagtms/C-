d6f334374bc9105ccd05e511b5e015b058858aac
sena <46827580+senagtms@users.noreply.github.com>
2019-07-03 14:53:22 +0300

    Add files via upload

---
+++ Guessing game/Guessing game/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3Tahmin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool bulundu = false;
+            Random sayiGen = new Random();
+            int bilgisayarinUrettigiSayi = sayiGen.Next(1, 100);
+            int tahminSayisi = 1;
+            Console.WriteLine("1-100 arasında tahminde bulun");
+            while (bulundu == false)//for(;bulundu==false;)
+            {
+                int tahmin = Convert.ToInt32(Console.ReadLine());
+                if (tahmin == bilgisayarinUrettigiSayi)
+                {
+                    Console.WriteLine("tebrikler {0} tahminde buldun", tahminSayisi);
+                    bulundu = true;
+                }
+                else
+                {
+                    tahminSayisi++;
+                    //ipucu ver
+                    if (tahmin > bilgisayarinUrettigiSayi)
+                        Console.WriteLine("tahmini kucult");
+                    else
+                        Console.WriteLine("tahmini buyut");
+                }
+            
+            }
+        }
+    }
+}

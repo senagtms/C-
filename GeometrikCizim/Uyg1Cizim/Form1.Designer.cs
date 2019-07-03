50bcd269fceb4269183bfefccc4f9c330e9074d8
sena <46827580+senagtms@users.noreply.github.com>
2019-07-03 15:29:37 +0300

    Add files via upload

---
+++ GeometrikCizim/Uyg1Cizim/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Uyg1Cizim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Çizgi Çiz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Elips Ciz";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 137);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "dikdortgen Çiz";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(123, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(259, 233);
+            this.panel1.TabIndex = 3;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(406, 13);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(382, 233);
+            this.panel2.TabIndex = 4;
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(406, 262);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(13, 262);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(369, 271);
+            this.panel3.TabIndex = 6;
+            this.panel3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel3_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 571);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ GeometrikCizim/Uyg1Cizim/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uyg1Cizim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics cizimAlani = panel1.CreateGraphics();
+            Pen Kalem = new Pen(Color.Cyan,3.1f);
+            cizimAlani.DrawLine(Kalem, 50, 60, 100, 200);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Graphics cizimAlani = panel1.CreateGraphics();
+            Pen kalem = new Pen(Color.Magenta, 4);
+            cizimAlani.DrawEllipse(kalem, 90, 85, 60, 60);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Graphics cizimAlani = panel1.CreateGraphics();
+            Pen kalem = new Pen(Color.Indigo, 5);
+            cizimAlani.DrawRectangle(kalem, 110, 100, 80, 90);
+        }
+
+        private void panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            Graphics cizimAlani = panel2.CreateGraphics();
+            Pen Kalem = new Pen(Color.Coral, 4);
+            cizimAlani.DrawRectangle(Kalem, e.X, e.Y, 50, 50);
+            label1.Text = e.X + "," + e.Y;
+            
+        }
+
+        private void panel2_MouseMove(object sender, MouseEventArgs e)
+        {
+            Graphics cizimAlani = panel2.CreateGraphics();
+            Pen Kalem = new Pen(Color.DarkOliveGreen, 0.5f);
+            cizimAlani.DrawEllipse(Kalem, e.X, e.Y, 5, 5);
+        }
+
+        int[] Xler = new int[3];
+        int[] Yler = new int[3];
+        int tS = 0;
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Xler[tS] = e.X;
+            Yler[tS] = e.Y;
+            tS++;
+            if (tS == 3)
+            {
+                //cizdir ve ts'yi sıfırla
+                Graphics cizimAlani = panel1.CreateGraphics();
+                Pen Kalem = new Pen(Color.BlueViolet, 2f);
+                cizimAlani.DrawLine(Kalem, Xler[0], Yler[0], Xler[1], Yler[1]);
+                cizimAlani.DrawLine(Kalem, Xler[1], Yler[1], Xler[2], Yler[2]);
+                cizimAlani.DrawLine(Kalem, Xler[2], Yler[2], Xler[0], Yler[0]);
+                tS = 0;
+
+            }
+        }
+
+        int[] Xler2 = new int[2];
+        int[] Yler2 = new int[2];
+        int tS2 = 0;
+        private void panel3_MouseDown(object sender, MouseEventArgs e)
+        {
+            Xler2[tS2] = e.X;
+            Yler2[tS2] = e.Y;
+            tS2++;
+            if (tS2 == 2)
+            {
+                Graphics cizimAlani = panel3.CreateGraphics();
+                Pen Kalem = new Pen(Color.Black, 2f);
+                cizimAlani.DrawLine(Kalem, Xler2[0], Yler2[0], Xler2[1], Yler2[1]);
+                tS2 = 0;
+            }
+        }
+    }
+}
